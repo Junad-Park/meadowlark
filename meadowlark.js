9c0000396486a7da65dfb261795251e0ac01d0a6
Junad-Park <qkrwnsgk1624@icloud.com>
2022-12-31 16:55:14 +0900

    test : add test code

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-const fortune = require("./lib/fortune");
+const handlers = require("./lib/handlers");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,29 +15,19 @@ app.engine(
 app.set("view engine", "handlebars");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
+app.get("/", handlers.home);
+app.get("/about", handlers.about);
 
-app.get("/about", (req, res) => {
-    res.render("about", { fortune: fortune.getFortune() });
-});
+app.use(handlers.notFound);
+app.use(handlers.serverError);
 
-// custom 404 page
-app.use((req, res) => {
-    res.status(404);
-    req.render("404");
-});
-// custom 500 page
-app.use((req, res) => {
-    console.error(err.message);
-    res.status(500);
-    req.render("500");
-});
-
-app.listen(port, () =>
-    console.log(
-        `Express started on https://localhost:${port}; ` +
-            `press Ctrl-C to terminate.`
-    )
-);
+if (require.main === module) {
+    app.listen(port, () =>
+        console.log(
+            `Express started on https://localhost:${port}; ` +
+                `press Ctrl-C to terminate.`
+        )
+    );
+} else {
+    module.exports = app;
+}
