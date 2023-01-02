67486bc122434af2c08f53203291a49580b1433d
Junad-Park <qkrwnsgk1624@icloud.com>
2023-01-02 16:37:48 +0900

    feat : add section

--- meadowlark.js
+++ meadowlark.js
@@ -9,14 +9,25 @@ app.engine(
     "handlebars",
     expressHandlebars({
         defaultLayout: "main",
+        helpers: {
+            section: function (name, options) {
+                if (!this._section) this._sections = {};
+                this._sections[name] = options.fn(this);
+                return null;
+            },
+        },
     })
 );
+app.disable("x-powered-by");
 
 app.set("view engine", "handlebars");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", handlers.home);
 app.get("/about", handlers.about);
+app.get("/greeting", handlers.greeting);
+app.get("/no-layout", handlers.noLayout);
+app.get("/section-test", handlers.sectionTest);
 
 app.use(handlers.notFound);
 app.use(handlers.serverError);
